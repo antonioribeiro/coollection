c438fadc2c99688a4348be34bea1849c56434381
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-12-02 14:39:24 -0200

    Automatically get the newest Collect version from github

--- upgrade-collect.sh
+++ upgrade-collect.sh
@@ -6,29 +6,13 @@
 shopt -s dotglob
 
 ##
- # Define all variables
- #
-collectVersion=5.5.20
-homeDir=.
-baseDir=src/vendor/tightenco
-oldNamespace='Illuminate\\'
-newNamespace='Tightenco\\Collect\\'
-
-collectDir=${baseDir}/collect
-repositoryDir=${collectDir}/collect-${collectVersion}
-collectZip=${collectDir}/collect.zip
-collectZipUrl=https://github.com/tightenco/collect/archive/v${collectVersion}.zip
-srcDir=${collectDir}/src
-tightencoNamespaceDir=${srcDir}/Tightenco/Collect
-illuminateNamespaceDir=${srcDir}/Illuminate
-
-
-##
  # App
  #
 function main()
 {
-    echo "Upgrading Collect..."
+    echo "-- Upgrading Collect..."
+
+    prepareEnvironment
 
     displayVariables
 
@@ -50,8 +34,9 @@ function main()
  #
 function displayVariables()
 {
-    echo "Variables"
-    echo "---------"
+    echo
+    echo "-- Variables"
+    echo "---------------------------------------------"
 
     echo collectDir = ${collectDir}
     echo collectVersion = ${collectVersion}
@@ -60,6 +45,8 @@ function displayVariables()
     echo srcDir = ${srcDir}
     echo oldNamespace = ${oldNamespace}
     echo newNamespace = ${newNamespace}
+
+    echo "---------------------------------------------"
 }
 
 ##
@@ -68,7 +55,7 @@ function displayVariables()
 function createDir()
 {
     if [ -d ${baseDir} ]; then
-        echo "Cleaning ${baseDir}"
+        echo "-- Cleaning ${baseDir}"
 
         rm -rf ${baseDir}
     fi
@@ -81,7 +68,7 @@ function createDir()
  #
 function download()
 {
-    echo "Downloading ${collectZipUrl} to ${collectDir}"
+    echo "-- Downloading ${collectZipUrl} to ${collectDir}"
 
     wget ${collectZipUrl} -O ${collectZip} >/dev/null 2>&1
 }
@@ -91,7 +78,7 @@ function download()
  #
 function extract()
 {
-    echo "Extracting collect.zip..."
+    echo "-- Extracting collect.zip..."
 
     unzip ${collectZip} -d ${collectDir} >/dev/null 2>&1
 
@@ -107,7 +94,7 @@ function extract()
  #
 function cleanupDir()
 {
-    echo "Cleaning up Tightenco dir..."
+    echo "-- Cleaning up Tightenco dir..."
 
     mv ${collectDir}/src ${collectDir}/../
 
@@ -119,9 +106,19 @@ function cleanupDir()
 ##
  # Copy tests to our tests dir
  #
+function getCurrentCollectVersion()
+{
+    collectVersion=$(git ls-remote https://github.com/tightenco/collect.git | grep tags/ | grep -v {} | cut -d \/ -f 3 | cut -d v -f 2 | sort --version-sort | tail -1)
+
+    echo Upgrading to tightenco/collect $collectVersion
+}
+
+##
+ # Copy tests to our tests dir
+ #
 function copyTests()
 {
-    echo "Copying tests..."
+    echo "-- Copying tests..."
 
     cp ${collectDir}/tests/Support/SupportCollectionTest.php ${homeDir}/tests/
 }
@@ -131,7 +128,7 @@ function copyTests()
  #
 function renameNamespace()
 {
-    echo "Renaming namespace from Illuminate to Tightenco..."
+    echo "-- Renaming namespace from Illuminate to Tightenco..."
 
     mkdir -p ${tightencoNamespaceDir}
 
@@ -140,6 +137,27 @@ function renameNamespace()
     rmdir ${illuminateNamespaceDir}
 
     find ${baseDir} -name "*.php" -exec sed -i "" -e "s|${oldNamespace}|${newNamespace}|g" {} \;
+}
+
+function prepareEnvironment()
+{
+    getCurrentCollectVersion
+
+    ##
+     # Define all variables
+     #
+    homeDir=.
+    baseDir=src/vendor/tightenco
+    oldNamespace='Illuminate\\'
+    newNamespace='Tightenco\\Collect\\'
+
+    collectDir=${baseDir}/collect
+    repositoryDir=${collectDir}/collect-${collectVersion}
+    collectZip=${collectDir}/collect.zip
+    collectZipUrl=https://github.com/tightenco/collect/archive/v${collectVersion}.zip
+    srcDir=${collectDir}/src
+    tightencoNamespaceDir=${srcDir}/Tightenco/Collect
+    illuminateNamespaceDir=${srcDir}/Illuminate
 }
 
 ##
